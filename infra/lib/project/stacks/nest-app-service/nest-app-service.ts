83bde897f2c5c454e1bbdf49f00444b647a2ecee
Altynbek Anarbekov <altynbek_anarbekov@epam.com>
2024-06-13 02:04:56 +0600

    finish task 8

--- infra/lib/project/stacks/nest-app-service/nest-app-service.ts
+++ infra/lib/project/stacks/nest-app-service/nest-app-service.ts
@@ -5,8 +5,10 @@ import { Construct } from 'constructs';
 import { StackProps } from 'aws-cdk-lib';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import * as rds from 'aws-cdk-lib/aws-rds';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
-const LAMBDA_PATH = '../../alt-rs-cart-api/dist';
+const LAMBDA_PATH = '../../nodejs-aws-cart-api/dist';
 
 export class NestJsLambdaStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -14,6 +16,14 @@ export class NestJsLambdaStack extends cdk.Stack {
 
     const vpc = new ec2.Vpc(this, 'MyVpc', {
       maxAzs: 2, // Default is all AZs in the region
+      natGateways: 0,
+      subnetConfiguration: [
+        {
+          cidrMask: 24,
+          name: 'PublicSubnet',
+          subnetType: ec2.SubnetType.PUBLIC,
+        },
+      ],
     });
 
     const securityGroup = new ec2.SecurityGroup(this, 'DatabaseSecurityGroup', {
@@ -31,10 +41,6 @@ export class NestJsLambdaStack extends cdk.Stack {
       engine: rds.DatabaseInstanceEngine.postgres({
         version: rds.PostgresEngineVersion.VER_16,
       }),
-      // instanceType: ec2.InstanceType.of(
-      //   ec2.InstanceClass.BURSTABLE2,
-      //   ec2.InstanceSize.SMALL
-      // ),
       instanceType: ec2.InstanceType.of(
         ec2.InstanceClass.T3,
         ec2.InstanceSize.MICRO
@@ -42,27 +48,44 @@ export class NestJsLambdaStack extends cdk.Stack {
       vpc,
       securityGroups: [securityGroup],
       databaseName: 'CartServiceDB',
-      credentials: rds.Credentials.fromGeneratedSecret('postgres'),
+      credentials: rds.Credentials.fromPassword(
+        process.env.DB_USERNAME as string,
+        cdk.SecretValue.plainText(process.env.DB_PASSWORD as string)
+      ),
+      vpcSubnets: {
+        subnetType: ec2.SubnetType.PUBLIC,
+      },
+      multiAz: false,
+      allocatedStorage: 20,
+      maxAllocatedStorage: 100,
+      allowMajorVersionUpgrade: false,
+      autoMinorVersionUpgrade: true,
+      backupRetention: cdk.Duration.days(7),
+      deleteAutomatedBackups: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      deletionProtection: false,
     });
 
     const nestLambda = new lambda.Function(this, 'CustomerCrudLambda', {
       runtime: lambda.Runtime.NODEJS_20_X,
       handler: 'main.handler',
       code: lambda.Code.fromAsset(LAMBDA_PATH),
+      vpc: vpc,
+      allowPublicSubnet: true,
+      securityGroups: [securityGroup],
+      // securityGroups: [...database.connections.securityGroups],
       environment: {
         NODE_ENV: 'production',
         DB_HOST: database.dbInstanceEndpointAddress,
         DB_PORT: database.dbInstanceEndpointPort,
-        // DB_USER:
-        //   database.secret?.secretValueFromJson('username')?.toString() ??
-        //   'Missing user',
-        // DB_PASSWORD:
-        //   database.secret?.secretValueFromJson('password')?.toString() ??
-        //   'Missing password',
         DB_SECRET_ARN: database.secret?.secretArn ?? 'No secret ARN',
-        DB_NAME: 'CartDB',
+        DB_NAME: 'CartServiceDB',
       },
+      timeout: cdk.Duration.seconds(20),
     });
+
+    database.connections.allowDefaultPortFrom(nestLambda);
+    database.secret?.grantRead(nestLambda);
 
     // Define API Gateway
     const api = new apigateway.LambdaRestApi(this, 'NestApi', {
